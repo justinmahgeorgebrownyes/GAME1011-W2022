8e38a73cc5a2c694712c285d8703149cf5371f60
justinmahgeorgebrownyes <justin.mah2@georgebrown.ca>
2022-02-10 19:29:20 -0500

    I created a outline for animation frame

--- milestone 1/AnimationFrame.h
+++ milestone 1/AnimationFrame.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class AnimationFrame
 {
+private:
+	string line1;
+	string line2;
+	string line3;
+
+public:
+	AnimationFrame(string line1, string line2, string line3) {
+
+		this->line1 = line1;
+		this->line2 = line2;
+		this->line3 = line3;
+
+	}
+
+	
 };
 
---
+++ milestone 1/Creature1.cpp
@@ -0,0 +1,1 @@
+#include "Creature1.h"
